f556392f6d779a643520b9d1bcff2a9bf56d360a
ziggyclout <idakiallan989@gmail.com>
2026-01-03 04:51:40 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task =
+      event.target.elements['new-task-description'].value;
+
+    buildToDo(task);
+    form.reset();
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement('li');
+  li.textContent = task;
+
+
+  const taskList = document.getElementById('tasks');
+  taskList.appendChild(li);
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,6 @@
+
+
+
 const chai = require('chai');
 global.expect = chai.expect;
 
