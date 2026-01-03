4cd10b40d62fc7628142c76ee9cd5f59d9389857
ziggyclout <idakiallan989@gmail.com>
2026-01-03 06:11:44 +0300

    Fix task submission: correct input variable and remove whitespace in tasks UL

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    // Grab input value
     const taskInput = document.getElementById('new-task-description').value;
 
+    // Pass correct variable
     buildToDo(taskInput);
+
+    // Reset input field
     form.reset();
   });
 });
@@ -15,7 +19,7 @@ function buildToDo(task) {
   const li = document.createElement('li');
   li.textContent = task;
 
-
+  
   const taskList = document.getElementById('tasks');
   taskList.appendChild(li);
 }
